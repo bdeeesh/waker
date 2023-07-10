f808b3ea8610591e879ca578e6d2e7ff54ec8c27
bdeeesh <osama.emohsen@gmail.com>
2023-07-10 08:35:10 -0500

    First commit. Adding code to repo.

---
+++ PyScope.py
@@ -0,0 +1,217 @@
+from RsInstrument import *  # The RsInstrument package is hosted on pypi.org, see Readme.txt for more details
+
+resource_string_1 = 'TCPIP::131.225.137.193::inst0::INSTR'  # Standard LAN connection (also called VXI-11)
+resource_string_2 = 'TCPIP::131.225.137.193::hislip0::INSTR'  # Hi-Speed LAN connection - see 1MA208
+
+import sys
+
+
+#LOG = 0
+
+#name formate FILENAME_RUN.Wfm.bin
+#
+
+class Control(object):
+
+    def __init__(self,FileName,N,instrAdd=resource_string_1,Location='"C:\Temp\\',FileFormat = '.bin',LOG=0):
+        ''' 
+        this function establish connection with the instrument at address resource_string_1
+        FileName : rootname for the measurment file
+        Location : rootlocation 
+        FileFormat : bin with int,8 
+        '''
+        self.instrAdd = instrAdd
+        self.N = N
+
+        self.Location = Location
+        self.FileFormat = FileFormat
+
+        self.File = str(FileName)
+
+        self.FileLocation = self.Location+str(self.File)#+'"'
+        print (self.FileLocation)
+
+
+        try:
+            self.instr = RsInstrument(self.instrAdd,"VisaTimeout=60000",reset=False)#,"OpcTimeout=60000","OpcWaitMode=OpcQuery")
+            #self.instr.VisaTimeout = 60000
+            #self.instr.OpcTimeout = 60000
+            self.instr.instrument_status_checking=True
+
+            if LOG: self.instr.logger.log_to_console = True; self.instr.logger.mode = LoggingMode.On
+
+        except Exception as ex:
+            print('Error initializing the instrument session:\n' + ex.args[0])
+            exit()
+
+
+    def close(self):
+        self.instr.close()
+
+    def preP(self,dtype='INT,8'):
+        #COMM = 'SYSTem:DISPlay:UPDate ON'
+        #self.instr.write_str(COMM)
+        # Normal mode Trig
+        self.instr.write_str('TRIG1:MODE NORM')
+        # Exter mode Trig
+        self.instr.write_str('TRIG1:SOUR EXT')
+        # INT 8 data (binary)
+        COMM = 'FORMat:DATA INT,8'
+        self.instr.write(COMM)
+
+    
+        self.instr.query_opc()
+
+    def reset(self):
+        self.instr.write('*RST')
+
+
+    def setVscale(self,CHNum,SCALE,POS=0,OFF=0):
+        CHAN = 'CHAN'+str(CHNum)
+        COMM = CHAN+':POS '+str(POS)
+        self.instr.write(COMM)
+
+        COMM = CHAN+':OFFS '+str(OFF)
+        self.instr.write(COMM)
+
+        COMM = CHAN+':SCALE '+str(SCALE)
+        self.instr.write(COMM)
+
+        COMM = CHAN+':STATe ON' # turn on CH
+        self.instr.write_str(COMM)
+
+        self.instr.query_opc()
+
+    def setHscale(self,SCALE,POS=0,OFF=0):
+        COMM = 'TIM:SCAL '+str(SCALE)
+        self.instr.write_str(COMM)
+        
+        self.instr.query_opc()
+
+
+    def acqSetting(self,multiCH=True,RES=100e-12):
+        COMM = 'EXPort:WAVeform:FASTexport ON'
+        self.instr.write_str(COMM)
+        if multiCH :
+            COMM = 'EXPort:WAVeform:MULTichannel ON'
+            self.instr.write_str(COMM)
+        else:
+            COMM = 'CHANnel1:WAVeform1:STATe 1'
+            self.instr.write_str(COMM)
+
+        #COMM = 'ACQuire:SEGMented:MAX ON'
+        #self.instr.write_str(COMM)
+        #disabeled for now
+        
+        COMM = 'ACQuire:COUNt '+str(self.N)
+        self.instr.write_str(COMM)
+
+        COMM = 'ACQ:RES '+str(RES)
+        self.instr.write_str(COMM)
+
+        COMM = 'ACQ:SEGM:STAT ON'
+        self.instr.write_str(COMM)
+
+        self.instr.query_opc()
+
+
+    def runSingle(self):
+        COMM = 'SING'
+        self.instr.write_str(COMM)
+        self.instr.query_opc(70000)
+
+
+
+    def deleteData(self,run):
+
+        CCOMM = 'MMEM:DEL '+self.FileLocation
+
+        self.instr.write_str(COMM)
+
+
+    def exportSetting(self,run):
+
+        COMM = 'EXPort:WAVeform:SOURce C1W1'
+        self.instr.write_str(COMM)
+        COMM = 'EXPort:WAVeform:SCOPe WFM'
+        self.instr.write_str(COMM)
+        RUN = str(run).zfill(3)
+
+        outputFile = self.FileLocation+RUN+self.FileFormat+'"'
+        print (outputFile)
+
+        self.instr.query_opc()
+        COMM = 'EXPort:WAVeform:NAME '+outputFile
+        self.instr.write_str(COMM)
+        COMM = 'EXPort:WAVeform:RAW ON'
+        self.instr.write_str(COMM)
+        COMM = 'EXPort:WAVeform:INCXvalues OFF'
+        self.instr.write_str(COMM)
+        COMM = 'EXPort:WAVeform:DLOGging ON'
+        self.instr.write_str(COMM)
+        self.instr.query_opc()
+
+
+
+    def playHistory(self,Hi,Hf=0):
+        Hi = (-1*Hi) +1
+        COMM = 'CHANnel1:WAV1:HISTory:STATe ON'
+        self.instr.query_opc()
+        self.instr.write_str(COMM)
+        COMM = 'CHANnel1:WAV1:HISTory:TPAC 40e-6'
+        self.instr.write_str(COMM)
+        COMM = 'CHANnel1:WAV1:HISTory:STARt '+str(Hi)
+        self.instr.write_str(COMM)
+        COMM = 'CHANnel1:WAV1:HISTory:STOP '+str(Hf)
+        self.instr.write_str(COMM)
+        COMM = 'CHANnel1:WAV1:HISTory:REPLay OFF'
+        self.instr.write_str(COMM)
+        COMM = 'CHANnel1:WAV1:HISTory:PLAY'
+        self.instr.write_str(COMM)
+        self.instr.query_opc(70000)
+
+    def waitForCommand(self):
+        self.instr.query_opc()
+
+
+    def copyFile(self,run):
+        self.instr.query_opc()
+        RUN = str(run).zfill(3)
+        self.FileLocation = self.FileLocation.strip('"')
+        
+
+        File0 = self.FileLocation+RUN+'.Wfm'+self.FileFormat
+        File1 = self.FileLocation+RUN+self.FileFormat
+        Local0 = self.File+RUN+'.Wfm'+self.FileFormat
+        Local1 = self.File+RUN+self.FileFormat 
+        print ('transfering files {} and {}'.format(File0,File1))
+        self.instr.read_file_from_instrument_to_pc(File1,Local1)
+        self.instr.query_opc()
+        self.instr.read_file_from_instrument_to_pc(File0,Local0)
+        self.instr.query_opc()
+        print ('done')
+
+
+
+    def copyFileName(self,fileName):
+
+        self.instr.query_opc()
+        FileLocationName = self.Location.strip('"')
+        
+        FileLocationName = self.Location+str(fileName)#+'"'
+        print (FileLocationName)
+
+
+        File0 = FileLocationName+'.Wfm'+self.FileFormat
+        File1 = FileLocationName+self.FileFormat
+        File0 = File0.strip('"')
+
+        File1 = File1.strip('"')
+        Local0 = fileName+'.Wfm'+self.FileFormat
+        Local1 = fileName+self.FileFormat 
+        print ('transfering files {} and {}'.format(File0,File1))
+        self.instr.read_file_from_instrument_to_pc(File1,Local1)
+        self.instr.query_opc()
+        self.instr.read_file_from_instrument_to_pc(File0,Local0)
+        self.instr.query_opc()
+        print ('done')
---
+++ readme.md
@@ -0,0 +1,14 @@
+Collection of scripts for the waker experiments. 
+
+PyScope.py : functions to control scope RTP at MI-60. 
+
+amplifierControl.py: scripts to control Amps at MI-20, from R.Ainsworth.
+
+ipm.py: scripts to get data from the ipm, from R.Ainsworth.
+
+scope.py: script to read wall current scope at MI-60, from R.Ainsworth.
+
+diagn.py: several classes that glues together ipm.py,scope.py and getRbeam.acl to simplify getting data during experiments. Examples are getSomeData.py
+
+scopeClass.py : class to analyze data from the scope. Converts data based on wfm and bin files from the scope into voltage data. 
+
